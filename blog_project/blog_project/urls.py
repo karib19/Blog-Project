d06e0a989a1bb900e398024d97847622570cbc08
karib19 <prokarib47@gmail.com>
2026-07-11 19:45:09 +0600

    urls.py updated

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -16,8 +16,7 @@ urlpatterns = [
     path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT,
-    )
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT,
+)
