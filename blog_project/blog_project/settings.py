c2454ef9835a0974fe53f7b83ae5e7590df084c7
karib19 <prokarib47@gmail.com>
2026-07-12 11:23:20 +0600

    Fix production settings

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-xo8whsumplgsbhn!-=^=@hrkukxk)@z(v6wgq$)=!htx7gbq+9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
