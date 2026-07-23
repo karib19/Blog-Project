b932c02f7c16cff2b4e462fc33639147c06d57ea
karib19 <prokarib47@gmail.com>
2026-07-23 12:38:52 +0600

    Debug register error

--- blog_project/blog_project/settings.py
+++ blog_project/blog_project/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    "rest_framework_simplejwt",
     'corsheaders',
     'django_filters',
     'drf_spectacular',
