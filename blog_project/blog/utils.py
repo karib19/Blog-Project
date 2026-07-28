b0f5dc3b18df4bd3de13c8b60ba9d1f36bb89a68
karib19 <prokarib47@gmail.com>
2026-07-28 12:05:27 +0600

    User fix

--- blog_project/blog/utils.py
+++ blog_project/blog/utils.py
@@ -3,28 +3,29 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 from .models import EmailOTP
-
+import traceback
 
 def generate_otp():
     return str(random.randint(100000, 999999))
 
 
 def send_otp_email(user):
-    EmailOTP.objects.filter(
-        user=user,
-        is_verified=False
-    ).delete()
+    try:
+        EmailOTP.objects.filter(
+            user=user,
+            is_verified=False
+        ).delete()
 
-    otp = generate_otp()
+        otp = generate_otp()
 
-    EmailOTP.objects.create(
-        user=user,
-        otp=otp
-    )
+        EmailOTP.objects.create(
+            user=user,
+            otp=otp
+        )
 
-    send_mail(
-        subject="Verify Your Email",
-        message=f"""
+        send_mail(
+            subject="Verify Your Email",
+            message=f"""
 Hello {user.username},
 
 Your OTP is:
@@ -33,7 +34,11 @@ Your OTP is:
 
 This OTP will expire in 5 minutes.
 """,
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[user.email],
-        fail_silently=False,
-    )
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email],
+            fail_silently=False,
+        )
+
+    except Exception:
+        traceback.print_exc()
+        raise
